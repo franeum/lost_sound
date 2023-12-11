98c443295ed2cf6c7635a5ce048586f500c26975
franeum <elettrochuck@gmail.com>
2023-12-11 23:38:46 +0100

    update

--- test_serial_read/test_serial_read.ino
+++ test_serial_read/test_serial_read.ino
@@ -1,22 +1,39 @@
-#include <TI_SN76489.h>
+// #include <TI_SN76489.h>
 
-TI_SN76489 sn(2, 3, 4, 5, 6, 7, 8, 10, 13, 9, _1MHz);
+// TI_SN76489 sn(2, 3, 4, 5, 6, 7, 8, 10, 13, 9, _1MHz);
+
+#define DEBUG
 
 void setup()
 {
-  Serial.begin(115200);
-  sn.begin();
-  sn.attenuation(0, 0);
-  sn.frequency(0, 200);
+    Serial.begin(115200);
+    /*
+    sn.begin();
+    sn.attenuation(0, 0);
+    sn.frequency(0, 200);
+    */
 }
 
 void loop()
-{ 
-  while (Serial.available() > 0) {
-    int red = Serial.parseInt(); 
-    
-    if (Serial.read() == '\n') 
-      sn.frequency(0, red);
-      
-  }  
+{
+    while (Serial.available() > 0)
+    {
+        byte status = Serial.parseInt();
+        byte note = Serial.parseInt();
+        byte velocity = Serial.parseInt();
+        byte channel = NULL;
+        
+        if (Serial.read() == '\n')
+            // sn.frequency(0, red);
+            channel = status & 15;
+            
+#if defined(DEBUG)
+        Serial.print("[ GENERATOR ]\t");
+        Serial.print(channel);
+        Serial.print("\t[ ATTENUATION ]\t");
+        Serial.print(velocity);
+        Serial.print("\t[ FREQUENCY ]\t");
+        Serial.println(note);
+#endif
+    }
 }
